6b047313936073e3406a98ae9c12af093862839d
lexon@lexbuntu <co.liang.ol@gmail.com>
2016-05-24 23:31:20 -0400

    Changed docker image file name to quay.io/eris/*:arch-version

--- version/images_arm.go
+++ version/images_arm.go
@@ -5,14 +5,14 @@ import (
 )
 
 var (
-	ERIS_REG_DEF = "" //quay.io
+	ERIS_REG_DEF = "quay.io"
 	ERIS_REG_BAK = "" //dockerhub
 
-	ERIS_IMG_BASE = fmt.Sprintf("eris4iot/base:%s", ARCH_ARM)
-	ERIS_IMG_DATA = fmt.Sprintf("eris4iot/data:%s", ARCH_ARM)
-	ERIS_IMG_KEYS = fmt.Sprintf("eris4iot/keys:%s", ARCH_ARM)
-	ERIS_IMG_DB   = fmt.Sprintf("eris4iot/erisdb:%s", ARCH_ARM)
-	ERIS_IMG_PM   = fmt.Sprintf("eris4iot/epm:%s", ARCH_ARM)
-	ERIS_IMG_CM   = fmt.Sprintf("eris4iot/eris-cm:%s", ARCH_ARM)
-	ERIS_IMG_IPFS = fmt.Sprintf("eris4iot/ipfs:%s", ARCH_ARM)
+	ERIS_IMG_BASE = fmt.Sprintf("eris/base:%s", ARCH_ARM)
+	ERIS_IMG_DATA = fmt.Sprintf("eris/data:%s", ARCH_ARM)
+	ERIS_IMG_KEYS = fmt.Sprintf("eris/keys:%s", ARCH_ARM)
+	ERIS_IMG_DB   = fmt.Sprintf("eris/erisdb:%s-%s", ARCH_ARM, VERSION)
+	ERIS_IMG_PM   = fmt.Sprintf("eris/epm:%s-%s", ARCH_ARM, VERSION)
+	ERIS_IMG_CM   = fmt.Sprintf("eris/eris-cm:%s-%s", ARCH_ARM, VERSION)
+	ERIS_IMG_IPFS = fmt.Sprintf("eris/ipfs:%s", ARCH_ARM)
 )
--- version/version_arm.go
+++ version/version_arm.go
@@ -6,4 +6,4 @@ const DOCKER_VER_MIN = "1.8.0"
 //-$- Hypriot doesn't support 0.6.0 version docker machine -$-
 const DM_VER_MIN = "0.4.0"
 
-const ARCH_ARM = "armhf"
+const ARCH_ARM = "arm"
